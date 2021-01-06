37af4aba3a38cc0f9bc6cc0fe837571fa9cea703
davq23 <davidquinterogranadillo@gmail.com>
2021-01-06 03:07:33 -0400

    Switch from input to textarea tag

--- static/js/view/Chat.js
+++ static/js/view/Chat.js
@@ -50,10 +50,12 @@ export default class ChatView extends View {
         
         this.form = document.createElement("form"); 
         this.form.id = "message-form"
-        this.messageInput = document.createElement("input"); 
+        this.messageInput = document.createElement("textarea"); 
         this.messageInput.id = "message";
-        this.messageInput.placeholder = "Your message go here";
+        this.messageInput.placeholder = "Your message goes here";
         this.messageInput.autocomplete = "off";
+        this.messageInput.rows="5" 
+        this.messageInput.maxlength="1023"; 
 
         this.submitButton = document.createElement('button');
         this.submitButton.innerText = "Send";
@@ -75,44 +77,49 @@ export default class ChatView extends View {
         const self = this;
         
         return new Promise(function(resolve, reject) {
-            self.ws = new WebSocket("wss://infinite-ocean-38389.herokuapp.com/chat");
+            try {   
+                self.ws = new WebSocket("wss://infinite-ocean-38389.herokuapp.com/chat");
             
 
-            self.ws.onopen = function() {
-                this.send(JSON.stringify({body: "Hello people!!"}))
-                self.closeConMessage.innerText = ""
-                resolve()
-            }
-
-            self.ws.onclose = function(event) {
-                self.closeConMessage.innerText = "Connection closed, please refresh to reconnect"
-            }
-            
-            self.ws.onmessage = function(event) {
-                if (self.chat.children.length > 20) {
-                    self.eraseMessages(10);
+                self.ws.onopen = function() {
+                    this.send(JSON.stringify({body: "Hello people!!"}))
+                    self.closeConMessage.innerText = ""
+                    resolve()
                 }
-
-                const message = JSON.parse(event.data);
-
-                const messageElement = new Message(message.sendername, message.body, message.sendat, message.receivedat);           
-
-                const node = messageElement.asHTMLNode();
+    
+                self.ws.onclose = function(event) {
+                    self.closeConMessage.innerText = "Connection closed, please refresh to reconnect"
+                }
                 
-
-                if (self.chat.scrollTop + self.chat.clientHeight >= self.chat.scrollHeight) {
-                    self.chat.appendChild(node);        
-                    self.chat.scrollTop = node.offsetHeight + node.offsetTop;
-                } else {
-                    self.chat.appendChild(node);        
+                self.ws.onmessage = function(event) {
+                    if (self.chat.children.length > 20) {
+                        self.eraseMessages(10);
+                    }
+    
+                    const message = JSON.parse(event.data);
+    
+                    const messageElement = new Message(message.sendername, message.body, message.sendat, message.receivedat);           
+    
+                    const node = messageElement.asHTMLNode();
+                    
+    
+                    if (self.chat.scrollTop + self.chat.clientHeight >= self.chat.scrollHeight) {
+                        self.chat.appendChild(node);        
+                        self.chat.scrollTop = node.offsetHeight + node.offsetTop;
+                    } else {
+                        self.chat.appendChild(node);        
+                    }
                 }
+    
+                self.ws.onerror = function(err) {
+                    self.chat.innerHTML += `<h3 class="message">An error occured!</h3>`;
+                    reject(err)
+                }
+                
+            } catch(err) {
+                reject(err);
             }
-
-            self.ws.onerror = function(err) {
-                self.chat.innerHTML += `<h3 class="message">An error occured!</h3>`;
-                reject(err)
-            }
-            
+           
             
         })
         
